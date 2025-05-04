689de4e0262a6e7fdb4bb63fe5e7a0d3553036f5
Servando <swyswy214@gmail.com>
2025-05-04 15:44:15 -0500

    chore: Init

--- backend/src/mongo.ts
+++ backend/src/mongo.ts
@@ -1,25 +1,40 @@
-import {MongoClient} from 'mongodb';
+import {Db, Document, MongoClient} from 'mongodb';
 
 export class MongoRepo {
-    private static instance: MongoRepo;
-    private mongoClient: MongoClient;
+  private static _instance: MongoRepo;
+  private readonly _mongoClient: MongoClient;
+  private readonly _db: Db;
 
-    private constructor() {
-        this.mongoClient = new MongoClient('mongodb://localhost:27017');
-    }
+  private constructor() {
+    this._mongoClient = new MongoClient(process.env['MONGO_URL'] ?? 'mongodb://localhost:27017');
+    this._db = this._mongoClient.db(process.env['MONGO_DB'] ?? 'test');
+    this._mongoClient.on('connect', () => {
+      console.log('MongoDB connected');
+    }).on('connectionReady', () => {
+      console.log('MongoDB connection ready');
+    }).on('connectionClosed', () => {
+      console.log('MongoDB connection closed');
+    }).on('error', (error) => {
+      console.error('MongoDB connection error', error);
+    })
+  }
 
-    public static getInstance(): MongoRepo {
-        if (!MongoRepo.instance) {
-        MongoRepo.instance = new MongoRepo();
-        }
-        return MongoRepo.instance;
+  public static get instance(): MongoRepo {
+    if (!MongoRepo._instance) {
+      MongoRepo._instance = new MongoRepo();
     }
+    return MongoRepo._instance;
+  }
 
-    public async connect() {
-        // Implement your connection logic here
-    }
+  public collection<T extends Document = Document>(name: string) {
+    return this._db.collection<T>(name);
+  }
 
-    public async disconnect() {
-        // Implement your disconnection logic here
+  static async connect() {
+    // Implement your connection logic here
+    if (!MongoRepo._instance) {
+      this._instance = new MongoRepo();
     }
+    await MongoRepo._instance._mongoClient.connect();
+  }
 }
